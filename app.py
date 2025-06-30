bda562221e8c85f4133d0721b033a60af7f37260
Shaun Fulton <fulton_shaun@yahoo.com>
2025-06-30 15:53:14 -0400

    refactor in progress

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
-import random
-import time
+# import random
+# import time
 
 
-sentences = [
-    "The quick brown fox jumps over the lazy dog.",
-    "Typing fast takes practice and focus.",
-    "Python makes programming fun and productive."
-]
+# sentences = [
+#     "The quick brown fox jumps over the lazy dog.",
+#     "Typing fast takes practice and focus.",
+#     "Python makes programming fun and productive."
+# ]
 
-target = random.choice(sentences)
+# target = random.choice(sentences)
 
-print("Type this:")
-print("\n" + target)
-input("Press Enter to start...")
+# print("Type this:")
+# print("\n" + target)
+# input("Press Enter to start...")
 
-start = time.time()
-typed = input("\nStart typing: ")
-end = time.time()
+# start = time.time()
+# typed = input("\nStart typing: ")
+# end = time.time()
 
-elapsed = round(end - start, 2)
+# elapsed = round(end - start, 2)
 
-words = len(target.split())
-wpm = round((words / elapsed) * 60)
+# words = len(target.split())
+# wpm = round((words / elapsed) * 60)
 
 # print(f"\n⏱ You took {elapsed} seconds")
 # print(f"⌨️  Your Speed: {wpm} words per minute")
